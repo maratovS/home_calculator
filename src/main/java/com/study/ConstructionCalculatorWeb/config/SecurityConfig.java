e4fe81d50bc6cfd8fd59f5fc0553f60e56d36004
reinner <serikssau@gmail.com>
2022-10-15 00:07:28 +0500

    T3-05-dev:[completed spring security features]

--- src/main/java/com/study/ConstructionCalculatorWeb/config/SecurityConfig.java
+++ src/main/java/com/study/ConstructionCalculatorWeb/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.study.ConstructionCalculatorWeb.config;
 
 import com.study.ConstructionCalculatorWeb.filter.CustomAuthenticationFilter;
+import com.study.ConstructionCalculatorWeb.filter.CustomAuthorizationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -28,13 +31,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
         http
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeRequests().anyRequest().permitAll()
+                .authorizeRequests().antMatchers("/api/login/**", "/api/token/refresh/**").permitAll()
                 .and()
-                .addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+                .authorizeRequests().antMatchers(HttpMethod.GET, "/api/**").hasAuthority("ROLE_MANAGER")
+                .and()
+                .authorizeRequests().antMatchers(HttpMethod.POST, "/api/**").hasAuthority("ROLE_MANAGER")
+                .and()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .addFilter(customAuthenticationFilter)
+                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
